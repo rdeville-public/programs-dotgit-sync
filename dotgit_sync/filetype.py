55b1cb9d897f0f9c0db7c3d890fdfffa76d4d7b3
Romain Deville <code@romaindeville.fr>
2024-06-30 22:32:54 +0200

    ➖ Remove external dependencies to libmagic

--- dotgit_sync/filetype.py
+++ dotgit_sync/filetype.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 """Check filetypes of template files."""
 
-import inspect  # noqa: I001
+import inspect
 import logging
 import pathlib
-
-import pylibmagic  # noqa: F401
-import magic
 
 from .utils import const
 
@@ -25,13 +22,11 @@ def get_filetype(filepath: pathlib.Path) -> str:
         String with the filetype
     """
     log.debug("%s.%s()", _LOG_TRACE, inspect.stack()[0][3])
-    ft = magic.from_file(filepath, mime=True).split("/")[-1]
-    if ft != "plain" and not ft.startswith("x-"):
-        return ft
-    if ft.startswith("x-"):
-        return ft.replace("x-", "")
     ext = "." + filepath.name.split(".")[-1]
     for ft, exts in const.FILETYPES.items():
         if ext in exts:
             return ft
+    log.warning(
+        "Unable to find a type for file %s, falling back to `plain`.", filepath
+    )
     return "plain"
