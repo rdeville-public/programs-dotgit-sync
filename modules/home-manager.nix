983fc90872980409748a75553e4031b36fb227ba
Romain Deville <code@romaindeville.fr>
2024-07-06 11:58:16 +0200

    ✨ Make home-manager module package install

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -2,12 +2,16 @@
 self: {
   pkgs,
   lib,
-# BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+  # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
   ...
 }: {
-  packages = [
-    self.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  ]
-# END DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
+  config = {
+    home = {
+      packages = [
+        self.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ];
+    };
+  };
+  # END DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
 }
 # END DOTGIT-SYNC BLOCK MANAGED
