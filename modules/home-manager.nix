93e2fd6235729ce22c13134d3ce6ed3e1d26c7ee
Romain Deville <code@romaindeville.fr>
2024-07-08 20:52:19 +0200

    ✨ Add option to enable package in HM module

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -2,10 +2,20 @@
 self: {
   pkgs,
   lib,
+  config,
   # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_HOME_MANAGER_MODULE_CUSTOM
   ...
-}: {
-  config = {
+}: let
+  cfg = config.dotgit-sync;
+in
+{
+  options = {
+    dotgit-sync = {
+      enable = lib.mkEnableOption "Install DotGit Sync Package";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
     home = {
       packages = [
         self.packages.${pkgs.stdenv.hostPlatform.system}.default
